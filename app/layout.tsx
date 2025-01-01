06430eaf37375f566cfc3411dc02744f837c2c46
thiri-10 <thiri7301@gmail.com>
2025-01-01 15:16:04 +0630

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Suspense } from "react";
-import Loading from "./components/loading";
+import Loading from "./components/Loading";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
